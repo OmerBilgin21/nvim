03838cae6522543f314bb44b7bee179860098ab9
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-08-31 19:04:28 +0200

    last update for lazyvim config update

--- lua/config/preset.lua
+++ lua/config/preset.lua
@@ -1,1 +1,40 @@
 vim.opt.guicursor = "i:blinkwait700-blinkoff400-blinkon250"
+_G.SetTheBackground = function()
+	local list_of_bffrs = {
+		"Normal",
+		"NormalNC",
+		"Comment",
+		"Constant",
+		"Special",
+		"Identifier",
+		"Statement",
+		"PreProc",
+		"Type",
+		"Underlined",
+		"Todo",
+		"String",
+		"Function",
+		"Conditional",
+		"Repeat",
+		"Operator",
+		"Structure",
+		"LineNr",
+		"NonText",
+		"SignColumn",
+		"CursorLine",
+		"CursorLineNr",
+		"StatusLine",
+		"StatusLineNC",
+		"EndOfBuffer",
+		--  here we could also set the following
+		--  NeoTreeNormal
+		--  NeoTreeNormalNC
+		-- However, I don't like the neo-tree transparent
+		-- so yeah, it's not
+	}
+	for _, item in ipairs(list_of_bffrs) do
+		vim.api.nvim_set_hl(0, item, { bg = "none" })
+	end
+end
+
+SetTheBackground()
--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,14 +1,14 @@
-function Yoman()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-    opts = function()
-      Yoman()
-    end,
-  },
+	{
+		-- "olimorris/onedarkpro.nvim",
+		"navarasu/onedark.nvim",
+		priority = 1000, -- Ensure it loads first
+		config = function()
+			-- require("onedark").setup({
+			-- 	style = "darker",
+			-- })
+			require("onedark").load()
+		end,
+		-- opts = function() end,
+	},
 }
