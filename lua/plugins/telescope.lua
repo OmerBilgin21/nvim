669fd39bbddcd7b9649f0d4dc4ac5428e1e8af56
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-01-18 17:03:07 +0100

    feat: use custom live grep commands and check for existence before access on telescope setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,44 +1,46 @@
-local telescope_builtin = require("telescope.builtin")
-local actions = require("telescope.actions")
-local telescope_config = require("telescope.config")
+local exists, _ = pcall(require, "telescope")
 
-local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
-table.insert(vimgrep_arguments, "--hidden")
-table.insert(vimgrep_arguments, "--glob")
-table.insert(vimgrep_arguments, "!**/.git/*")
-table.insert(vimgrep_arguments, "--glob")
-table.insert(vimgrep_arguments, "!*.lock.json")
+local vimgrep_args = {
+  "rg",
+  "--color=never",
+  "--no-heading",
+  "--with-filename",
+  "--line-number",
+  "--column",
+  "--smart-case",
+  "--glob",
+  "!node_modules/**",
+  "--glob",
+  "!.git/**",
+  "--glob",
+  "!*lock.json",
+}
 
-return {
+local return_table = {
   "nvim-telescope/telescope.nvim",
 
-  opts = {
-    pickers = {
-      find_files = {
-        find_command = {
-          "rg",
-          "--files",
-          "--hidden",
-          "--glob",
-          "!**/.git/*",
-          "--glob",
-          -- package.lock.json or yarn version
-          -- annoys me quite a bit
-          "!*.lock.json",
-        },
-      },
+  keys = {
+    { "<C-p>",            "<cmd>Telescope find_files<CR>" },
+    {
+      "<C-f>",
+      function()
+        local tbuiltin = require("telescope.builtin")
+        tbuiltin.live_grep({ vimgrep_arguments = vimgrep_args })
+      end,
     },
+    { "<leader><leader>", "<cmd>Telescope buffers initial_mode=normal theme=dropdown sort_mru=true<CR>" },
+    { "<leader>d",        "<cmd>Telescope help_tags<CR>" },
+  },
+}
+
+if exists then
+  local actions = require("telescope.actions")
+
+  return_table["opts"] = {
     defaults = {
       preview = {
-        -- quite dangerous to disable this
-        -- since I changed the config to recognize
-        -- the hidden files as well
         filesize_limit = 1,
       },
-      -- for live_grep since apparently it doesn't support
-      -- changing the flags natively
-      -- so I insert it above to a table and give it here
-      vimgrep_arguments = vimgrep_arguments,
       mappings = {
         i = {
           ["<C-c>"] = actions.close,
@@ -51,13 +53,7 @@ return {
         },
       },
     },
-  },
+  }
+end
 
-  keys = {
-
-    { "<C-p>",            "<cmd>Telescope find_files<CR>" },
-    { "<C-f>",            "<cmd>Telescope live_grep<CR>" },
-    { "<leader><leader>", "<cmd>Telescope buffers initial_mode=normal theme=dropdown sort_mru=true<CR>" },
-    { "<leader>th",       "<cmd>Telescope help_tags<CR>" },
-  },
-}
+return return_table
