9821b7549954c2bbf39a2d2de1228dc57f288ed1
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-01-18 17:03:57 +0100

    fix: add ensure_installed and rename to prevent overrides

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,11 +3,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
+      local ts_config = require("nvim-treesitter.configs")
+      ts_config.setup({
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        ensure_installed = { "lua", "javascript", "python", "jsonc", "prisma" },
       })
     end,
   },
