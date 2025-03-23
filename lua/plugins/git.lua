f565f8a59cba4d66806601a7fd8a5581ea5f9f18
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-03-23 22:12:16 +0100

    feat: assign shortcut to get ours and theirs hunks easier

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,6 +15,8 @@ return {
       "tpope/vim-fugitive",
       config = function()
         vim.keymap.set("n", "<leader>gm", "<cmd>:Gvdiffsplit!<cr>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>go", "<cmd>:diffget //2<cr>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>gt", "<cmd>:diffget //3<cr>", { noremap = true, silent = true })
       end,
     },
   },
