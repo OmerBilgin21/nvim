96bd4e57204d62a9e2a6f49402f5b938847dac2f
oemer-lendis <oemer.bilgin@lendis.de>
2025-04-05 10:21:19 +0200

    feat: remove unnecessary plugin since fugitive has a blame view

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,17 +1,8 @@
-vim.keymap.set("n", "<leader>gb", "<cmd>:GitBlameToggle<cr>", { noremap = true, silent = true })
 return {
-  {
-    "f-person/git-blame.nvim",
-    event = "VeryLazy",
-    opts = {
-      enabled = false,
-      date_format = "%m-%d-%Y",
-      virtual_text_column = 1,
-    },
-  },
   {
     "tpope/vim-fugitive",
     config = function()
+      vim.keymap.set("n", "<leader>gb", "<cmd>G blame<cr>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>gm", "<cmd>:Gvdiffsplit!<cr>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>go", "<cmd>:diffget //2<cr>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>gt", "<cmd>:diffget //3<cr>", { noremap = true, silent = true })
