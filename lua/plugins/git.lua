ae5d6179861bec156c4d30a4b7a64a222141e3fa
oemer-lendis <oemer.bilgin@lendis.de>
2025-04-07 16:09:56 +0200

    feat: remove unnecessary plugin use fugitive for blame and implement undo keymap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,18 +1,14 @@
-vim.keymap.set("n", "<leader>gb", "<cmd>:GitBlameToggle<cr>", { noremap = true, silent = true })
 return {
-  {
-    "f-person/git-blame.nvim",
-    event = "VeryLazy",
-    opts = {
-      enabled = false,
-      date_format = "%m-%d-%Y",
-      virtual_text_column = 1,
-    },
-  },
   {
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gm", "<cmd>:Gvdiffsplit!<cr>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>gb", "<cmd>:G blame<cr>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>gr", function()
+        local back = vim.fn.input("Enter how many times to take back:")
+        local cmd = string.format("G reset --soft HEAD~%s", back)
+        vim.cmd(cmd)
+      end, { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>go", "<cmd>:diffget //2<cr>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>gt", "<cmd>:diffget //3<cr>", { noremap = true, silent = true })
     end,
