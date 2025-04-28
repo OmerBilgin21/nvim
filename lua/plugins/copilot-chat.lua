f295a0fa23e73de715a108e499cff505661e57c8
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-04-29 00:44:18 +0200

    feat: only give current buffer as context and adjust chat window
    To get more coherent answers
    adjust chat window to be a third of the screen

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -175,9 +175,22 @@ return {
     prompts = prompts,
     model = "gpt-4o",
     context = {
-      "buffers",
+      "buffer",
     },
     mappings = mappings,
+    window = {
+      layout = "vertical", -- 'vertical', 'horizontal', 'float', 'replace', or a function that returns the layout
+      width = 0.3, -- fractional width of parent, or absolute width in columns when > 1
+      height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
+      -- Options below only apply to floating windows
+      relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
+      border = "single", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+      row = nil, -- row position of the window, default is centered
+      col = nil, -- column position of the window, default is centered
+      title = "Copilot Chat", -- title of chat window
+      footer = nil, -- footer of chat window
+      zindex = 1, -- determines if window is on top or below other floating windows
+    },
   },
   config = function(_, opts)
     local chat = require("CopilotChat")
